362692d731c5610e5ff6914ac76c0857c3b9b89d
nataliaptzk <nataliaptzk@gmail.com>
2020-04-13 13:08:05 +0100

    Added start scene

---
+++ PortalPlanetsUnityProject/Assets/Scripts/SpanCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpanCamera : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+
+    [SerializeField] private Vector3 _lookAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(_lookAt, transform.up, Time.deltaTime * _rotationSpeed);
+        transform.LookAt(Vector3.zero);
+    }
+}
---
+++ PortalPlanetsUnityProject/Assets/UIAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        LeanTween.scale(gameObject, new Vector3(1.2f, 1.2f), 0.2f).setEase(LeanTweenType.easeOutExpo);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.scale(gameObject, new Vector3(1f, 1f), 0.2f).setEase(LeanTweenType.easeOutExpo);
+    }
+}
