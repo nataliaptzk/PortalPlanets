7af0e1b095d8c674b3ac26f482c1c4c297ce26b0
nataliaptzk <nataliaptzk@gmail.com>
2020-04-13 15:53:06 +0100

    Pause menu

---
+++ PortalPlanetsUnityProject/Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused;
+    [SerializeField] private GameObject _pauseMenu;
+
+    private void Start()
+    {
+        isPaused = false;
+        _pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            ChangePauseState();
+        }
+    }
+
+
+    private void ChangePauseState()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            _pauseMenu.SetActive(false);
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            _pauseMenu.SetActive(true);
+            isPaused = true;
+        }
+    }
+}
